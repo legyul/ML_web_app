128ab199b943382095e2bf8f1971981d348b065e
namdarine <namdarine.code@gmail.com>
2025-03-16 16:16:31 -0500

    Update huggingface

--- src/lora_train.py
+++ src/lora_train.py
@@ -4,15 +4,17 @@ from peft import LoraConfig, get_peft_model
 import os
 
 MODEL_NAME = "mistralai/Mistral-7B-v0.1"
+HF_TOKEN = os.getenv("HUGGINGFACE_API_KEY")
 
 # Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_auth_token=HF_TOKEN)
 
 # Load model (Save memory by applying 4-bit quantization)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
     load_in_4bit=True,  # 4-bit quantization
-    device_map="auto"
+    device_map="auto",
+    use_auth_token=HF_TOKEN
 )
 
 # LoRA settings (set to work on CPU as well)
