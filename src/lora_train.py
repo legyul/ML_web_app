1124399feac322d3d0cbf87b6cb97860db563b2c
namdarine <namdarine.code@gmail.com>
2025-03-18 20:57:24 -0500

    Update model

--- src/lora_train.py
+++ src/lora_train.py
@@ -1,20 +1,28 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
-from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model
 import os
 
-MODEL_NAME = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
+MODEL_NAME = "mosaicml/mpt-1b-redpajama-200b"
 HF_TOKEN = os.getenv("HUGGINGFACE_API_KEY")
 
 os.environ["HF_HOME"] = "/tmp/hg_cache"
 
-#bnb_config = BitsAndBytesConfig(load_in_8bit = False, load_in_4bit = True) # Enable to run on CPU
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit = True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+    )
 
 # Load tokenizer
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=HF_TOKEN)
 
 # Load model
-model = AutoModel.from_pretrained(MODEL_NAME, token=HF_TOKEN)
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME,
+    torch_dtype=torch.float16,
+    device_map="auto",
+    token=HF_TOKEN)
 
 # LoRA settings (set to work on CPU as well)
 lora_config = LoraConfig(
