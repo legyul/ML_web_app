9e4804c2a2a1c3d925bdd0f0604d8e42b9ccb425
namdarine <namdarine.code@gmail.com>
2025-04-01 11:00:47 -0500

    edit

--- src/rag_qa.py
+++ src/rag_qa.py
@@ -49,7 +49,7 @@ def get_qa_pipeline(filename: str, model_choice: str):
 
         print("[DEBUG] Loading tokenizer...")
         tokenizer = AutoTokenizer.from_pretrained(base_model_path, cache_dir=HF_CACHE, use_fast=False, local_files_only=True)
-        base_model = AutoModelForCausalLM.from_pretrained(base_model_path, cache_dir=HF_CACHE, local_file_only=True)
+        base_model = AutoModelForCausalLM.from_pretrained(base_model_path, cache_dir=HF_CACHE, local_files_only=True)
         
         # Attach LoRA adapter
         model = PeftModel.from_pretrained(base_model, model_path, local_files_only=True)
